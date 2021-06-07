ed5d90431fa738d2af692b8823a598d771757f58
Oleg Petrov <o.m.petrov@ya.ru>
2021-06-07 22:56:17 +0300

    Rename methods in GUI View and Controller

--- gui.py
+++ gui.py
@@ -20,18 +20,18 @@ class View(ttk.Frame):
         self.controller = Controller(self, machine)
 
         self.grid(sticky="NEWS")
-        self.create_widgets()
-        self.set_weight(self)
+        self.__create_widgets()
+        self.__set_weight(self)
 
     @staticmethod
-    def set_weight(widget):
+    def __set_weight(widget):
         """Make widget's grid stretchable."""
         for column in range(widget.grid_size()[0]):
             widget.columnconfigure(column, weight=1)
         for row in range(widget.grid_size()[1]):
             widget.rowconfigure(row, weight=1)
 
-    def new_widget(self, cls, row: int, column: int, rowspan: int=1, colspan: int=1, parent=None, **kwargs):
+    def __new_widget(self, cls, row: int, column: int, rowspan: int=1, colspan: int=1, parent=None, **kwargs):
         """Create subwidget and place it in grid.
 
         :param cls: a `tkinter` widget class to construct
@@ -44,17 +44,17 @@ class View(ttk.Frame):
         x.grid(row=row, column=column, rowspan=rowspan, columnspan=colspan, sticky='NEWS')
         return x
 
-    def create_widgets(self):
+    def __create_widgets(self):
         """Create all the widgets."""
         self.bold = font.Font(weight='bold')
 
-        self.tape_frame = self.new_widget(ttk.Frame, 0, 0)
+        self.tape_frame = self.__new_widget(ttk.Frame, 0, 0)
         self.tape = []
         for i in range(self.tape_size):
             def to_reg(i=i):
-                return self.controller.tape_check(i)
+                return self.controller._tape_check(i)
             vc = self.register(to_reg)
-            a = self.new_widget(
+            a = self.__new_widget(
                 ttk.Entry, 0, i, parent=self.tape_frame,
                 width=1, justify='center',
                 textvariable=self.controller.tape[i],
@@ -64,48 +64,48 @@ class View(ttk.Frame):
         i = self.model.position - self.controller.tape_start
         if 0 <= i < self.tape_size:
             self.tape[i]['font'] = self.bold
-        self.set_weight(self.tape_frame)
+        self.__set_weight(self.tape_frame)
 
-        machine_settings = self.new_widget(ttk.Frame, 1, 0)
+        machine_settings = self.__new_widget(ttk.Frame, 1, 0)
 
-        vc = self.register(self.controller.alphabet_check)
-        self.alphabet = self.new_widget(
+        vc = self.register(self.controller._alphabet_check)
+        self.alphabet = self.__new_widget(
             ttk.Entry, 0, 0, parent=machine_settings,
             textvariable=self.controller.alphabet, validate='focusout',
             validatecommand=vc
         )
 
-        self.tacts = self.new_widget(ttk.Label, 0, 1, parent=machine_settings, textvariable=self.controller.tacts)
-        self.makestep = self.new_widget(ttk.Button, 0, 3, parent=machine_settings, text='Step', command=self.controller.step)
-        self.go = self.new_widget(ttk.Button, 0, 2, parent=machine_settings, text='Go', command=self.controller.go)
+        self.tacts = self.__new_widget(ttk.Label, 0, 1, parent=machine_settings, textvariable=self.controller.tacts)
+        self.make_step = self.__new_widget(ttk.Button, 0, 3, parent=machine_settings, text='_step', command=self.controller._step)
+        self._go = self.__new_widget(ttk.Button, 0, 2, parent=machine_settings, text='_go', command=self.controller._go)
 
-        self.set_weight(machine_settings)
+        self.__set_weight(machine_settings)
 
-        self.rules = self.new_widget(ttk.Frame, 2, 0)
-        self.controller.update_rules()
+        self.rules = self.__new_widget(ttk.Frame, 2, 0)
+        self.controller._update_rules()
 
-        file_io = self.new_widget(ttk.Frame, 3, 0)
-        self.load = self.new_widget(ttk.Button, 0, 0, parent=file_io, text='Load')
-        self.save = self.new_widget(ttk.Button, 0, 1, parent=file_io, text='Save')
-        self.set_weight(file_io)
+        file_io = self.__new_widget(ttk.Frame, 3, 0)
+        self.load = self.__new_widget(ttk.Button, 0, 0, parent=file_io, text='Load')
+        self.save = self.__new_widget(ttk.Button, 0, 1, parent=file_io, text='Save')
+        self.__set_weight(file_io)
 
-    def update_rules(self):
+    def _update_rules(self):
         """Recreate the rules table."""
         self.rules.destroy()
-        self.rules = self.new_widget(ttk.Frame, 2, 0)
-        self.new_widget(ttk.Label, 0, 0, colspan=2, parent=self.rules, text='q \ a')
+        self.rules = self.__new_widget(ttk.Frame, 2, 0)
+        self.__new_widget(ttk.Label, 0, 0, colspan=2, parent=self.rules, text='q \ a')
 
         for j, c in enumerate(self.model.alphabet):
-            self.new_widget(ttk.Label, 0, j + 2, parent=self.rules, text=c)
+            self.__new_widget(ttk.Label, 0, j + 2, parent=self.rules, text=c)
         for i, s in enumerate(self.model.rules):
             def to_reg1(s=s):
-                return self.controller.delete_state(s)
+                return self.controller._delete_state(s)
             vc = self.register(to_reg1)
-            self.new_widget(ttk.Button, i + 1, 0, parent=self.rules, text='x', command=vc)
+            self.__new_widget(ttk.Button, i + 1, 0, parent=self.rules, text='x', command=vc)
             def to_reg(s=s):
-                return self.controller.state_check(s)
+                return self.controller._state_check(s)
             vc = self.register(to_reg)
-            self.new_widget(
+            self.__new_widget(
                 ttk.Entry, i + 1, 1, parent=self.rules,
                 width=5, justify='center',
                 textvariable=self.controller.rules[s],
@@ -114,9 +114,9 @@ class View(ttk.Frame):
             )
             for j, c in enumerate(self.model.alphabet):
                 def to_reg(s=s, c=c):
-                    return self.controller.rule_check(s, c)
+                    return self.controller._rule_check(s, c)
                 vc = self.register(to_reg)
-                self.new_widget(
+                self.__new_widget(
                     ttk.Entry, i + 1, j + 2, parent=self.rules,
                     textvariable=self.controller.rules[s, c], validate='focusout',
                     validatecommand=vc
@@ -124,14 +124,14 @@ class View(ttk.Frame):
 
         lll = len(self.model.rules)
         def to_reg(s=''):
-            return self.controller.state_check(s)
+            return self.controller._state_check(s)
         vc = self.register(to_reg)
-        self.new_widget(
+        self.__new_widget(
             ttk.Entry, 1 + lll, 1, parent=self.rules,
             textvariable=self.controller.rules[''], validate='focusout',
             validatecommand=vc
         )
-        self.set_weight(self.rules)
+        self.__set_weight(self.rules)
 
 class Controller:
     """Stores and manipulates control variables for view–model communication."""
@@ -161,7 +161,7 @@ class Controller:
 
         self.stashed = dict()
 
-    def tape_check(self, i: int):
+    def _tape_check(self, i: int):
         """Check change to the tape.
 
         :param i: index of changed cell
@@ -180,7 +180,7 @@ class Controller:
         self.model.tape[self.tape_start + i] = new
         return True
 
-    def update_rules(self, remove=''):
+    def __update_rules(self, remove=''):
         """Add or remove entries for rules.
 
         :param remove: characters to remove from machine model as they are removed from the alphabet
@@ -210,14 +210,14 @@ class Controller:
             for c in remove:
                 self.stashed[state, c] = line.pop(c, '')
 
-        self.view.update_rules()
+        self.view._update_rules()
 
-    def delete_state(self, s: str):
+    def _delete_state(self, s: str):
         """Remove the state from the machine."""
         self.model.rules.pop(s)
-        self.update_rules()
+        self.__update_rules()
 
-    def state_check(self, s: str):
+    def _state_check(self, s: str):
         """Check state name to be unique.
 
         :param s: the state that is renamed or '' in case of adding a new state
@@ -232,10 +232,10 @@ class Controller:
         assert(old == '' or old in self.model.rules)
         line = self.model.rules.pop(old, dict())
         self.model.rules[new] = line
-        self.update_rules()
+        self.__update_rules()
         return True
 
-    def rule_check(self, state: str, char: str):
+    def _rule_check(self, state: str, char: str):
         """Check the rule entry to be correct triple.
 
         :param state: state
@@ -253,7 +253,7 @@ class Controller:
         self.model.rules[state][char] = result
         return True
 
-    def alphabet_check(self):
+    def _alphabet_check(self):
         """Check the alphabet entry to consist of unique symbols."""
         old = self.model.alphabet
         new = self.alphabet.get()
@@ -262,22 +262,22 @@ class Controller:
         if len(set(new)) == len(new):
             new += LAMBDA
             self.model.alphabet = new
-            self.update_rules([c for c in old if c not in new])
+            self.__update_rules([c for c in old if c not in new])
             return True
         self.alphabet.set(old[:-1])
         return False
 
-    def step(self):
+    def _step(self):
         """Advance the Turing machine one tact."""
         result = self.model.run(max_tacts=1)
-        self.update_tape(result)
+        self.__update_tape(result)
 
-    def go(self):
+    def _go(self):
         """Run the Turing machine till it stops or tacts limit exceeds."""
         result = self.model.run()
-        self.update_tape(result)
+        self.__update_tape(result)
 
-    def update_tape(self, result):
+    def __update_tape(self, result):
         """Update the tape."""
         if result['iterations'] == 0:
             return  # the machine has stopped
